ed32119fc0198c42b5531147afdbb8bb7cfa926e
peter <xe@kthx.de>
2017-10-03 01:19:37 +0200

    Expand test

--- test/dbcparser_test.cpp
+++ test/dbcparser_test.cpp
@@ -20,15 +20,35 @@ TEST_CASE("DBC file parser")
   CHECK(m.id == 0xC9);
   CHECK(m.signals_.size() == 4);
 
-  auto signal_it = dbc::find_signal(m, "VEL");
-  REQUIRE(signal_it != std::end(m.signals_));
-  auto signal = *signal_it;
-  CHECK(signal.pos == 12);
-  CHECK(signal.len == 14);
-  CHECK(signal.order == dbc::byte_order::intel);
-  CHECK(signal.sign == dbc::value_sign::unsigned_);
-  CHECK(Approx(signal.factor) == 0.01220703125);
-  CHECK(Approx(signal.offset) == 0);
-  CHECK(Approx(signal.minimum) == 0);
-  CHECK(Approx(signal.maximum) == 250);
+  SUBCASE("signal VEL")
+  {
+    auto signal_it = dbc::find_signal(m, "VEL");
+    REQUIRE(signal_it != std::end(m.signals_));
+    auto signal = *signal_it;
+    CHECK(signal.pos == 12);
+    CHECK(signal.len == 14);
+    CHECK(signal.order == dbc::byte_order::intel);
+    CHECK(signal.sign == dbc::value_sign::unsigned_);
+    CHECK(Approx(signal.factor) == 0.01220703125);
+    CHECK(Approx(signal.offset) == 0);
+    CHECK(Approx(signal.minimum) == 0);
+    CHECK(Approx(signal.maximum) == 250);
+    CHECK(signal.unit == "km/h");
+  }
+
+  SUBCASE("signal MODE")
+  {
+    auto signal_it = dbc::find_signal(m, "MODE");
+    REQUIRE(signal_it != std::end(m.signals_));
+    auto signal = *signal_it;
+    CHECK(signal.pos == 26);
+    CHECK(signal.len == 6);
+    CHECK(signal.order == dbc::byte_order::intel);
+    CHECK(signal.sign == dbc::value_sign::unsigned_);
+    CHECK(Approx(signal.factor) == 1);
+    CHECK(Approx(signal.offset) == 0);
+    CHECK(Approx(signal.minimum) == 10);
+    CHECK(Approx(signal.maximum) == 50);
+    CHECK(signal.unit == "");
+  }
 }
