7b6d90ad7d5abf454c24c35e2c8125a679fb1ba6
peter <xe@kthx.de>
2017-10-07 17:07:37 +0200

    Change UI to dark style

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,30 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QStyleFactory>
 
 
 int main(int argc, char *argv[])
 {
   QApplication a(argc, argv);
-  MainWindow w;
+  a.setStyle(QStyleFactory::create("Fusion"));
+
+  QPalette dark;
+  dark.setColor(QPalette::Window, QColor(53, 53, 53));
+  dark.setColor(QPalette::WindowText, Qt::white);
+  dark.setColor(QPalette::Base, QColor(25, 25, 25));
+  dark.setColor(QPalette::AlternateBase, QColor(53, 53, 53));
+  dark.setColor(QPalette::ToolTipBase, Qt::white);
+  dark.setColor(QPalette::ToolTipText, Qt::white);
+  dark.setColor(QPalette::Text, Qt::white);
+  dark.setColor(QPalette::Button, QColor(53, 53, 53));
+  dark.setColor(QPalette::ButtonText, Qt::white);
+  dark.setColor(QPalette::BrightText, Qt::red);
+  dark.setColor(QPalette::Link, QColor(42, 130, 218));
+  dark.setColor(QPalette::Highlight, QColor(254,165,0));
+  dark.setColor(QPalette::HighlightedText, Qt::black);
+  a.setPalette(dark);
+
+  Main_window w;
   w.show();
 
   return a.exec();
