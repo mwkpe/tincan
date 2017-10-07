d280e3b509f147f8b7856c0d2c783fbf72b63c2f
peter <xe@kthx.de>
2017-10-07 17:06:51 +0200

    Add signal and apply PPP style

--- src/canreceiver.cpp
+++ src/canreceiver.cpp
@@ -5,14 +5,15 @@
 #include <QNetworkDatagram>
 
 
-void can::receiver::handle_received_datagram(const QNetworkDatagram* datagram)
+void can::Receiver::handle_received_datagram(const QNetworkDatagram* datagram)
 {
   using namespace std::chrono;
   if (datagram && datagram->isValid()) {
     auto data = datagram->data();
-    if (data.size() == sizeof(can::frame)) {
+    if (data.size() == sizeof(can::Frame)) {
       emit received_frame(duration_cast<milliseconds>(high_resolution_clock::now()
-          .time_since_epoch()).count(), *reinterpret_cast<can::frame*>(data.data()));
+          .time_since_epoch()).count(), *reinterpret_cast<can::Frame*>(data.data()));
+      emit received_frame_id(reinterpret_cast<can::Frame*>(data.data())->id);
     }
   }
 }
