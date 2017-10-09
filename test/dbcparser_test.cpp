c4da1673983ee37823af29e538333bb5639a0bdf
peter <xe@kthx.de>
2017-10-09 19:54:51 +0200

    Fix test

--- test/dbcparser_test.cpp
+++ test/dbcparser_test.cpp
@@ -26,8 +26,8 @@ TEST_CASE("DBC file parser")
     REQUIRE(signal_def != nullptr);
     CHECK(signal_def->pos == 12);
     CHECK(signal_def->len == 14);
-    CHECK(signal_def->order == dbc::byte_order::intel);
-    CHECK(signal_def->sign == dbc::value_sign::unsigned_);
+    CHECK(signal_def->order == dbc::Byte_order::Intel);
+    CHECK(signal_def->sign == dbc::Value_sign::Unsigned);
     CHECK(Approx(signal_def->factor) == 0.01220703125);
     CHECK(Approx(signal_def->offset) == 0);
     CHECK(Approx(signal_def->minimum) == 0);
@@ -41,8 +41,8 @@ TEST_CASE("DBC file parser")
     REQUIRE(signal_def != nullptr);
     CHECK(signal_def->pos == 26);
     CHECK(signal_def->len == 6);
-    CHECK(signal_def->order == dbc::byte_order::intel);
-    CHECK(signal_def->sign == dbc::value_sign::unsigned_);
+    CHECK(signal_def->order == dbc::Byte_order::Intel);
+    CHECK(signal_def->sign == dbc::Value_sign::Unsigned);
     CHECK(Approx(signal_def->factor) == 1);
     CHECK(Approx(signal_def->offset) == 0);
     CHECK(Approx(signal_def->minimum) == 10);
