37757b7dec31f9b7dd440b1e60731794196a588c
peter <xe@kthx.de>
2017-10-11 01:41:11 +0200

    Add signal raw value

--- src/models/bussignalitem.cpp
+++ src/models/bussignalitem.cpp
@@ -1,14 +1,18 @@
 #include "bussignalitem.h"
 
 
+#include <cstdint>
 #include <QString>
 
 
 QVariant tin::Bus_signal_item::data(int column) const
 {
   switch (column) {
-    case 0: return bus_signal_.signal_def ? QString::fromStdString(bus_signal_.signal_def->name)
-        : QVariant{};
+    case 0: return QString::fromStdString(bus_signal_->signal_def->name);
+    case 3:
+      if (std::holds_alternative<std::uint64_t>(bus_signal_->raw))
+        return std::get<std::uint64_t>(bus_signal_->raw);
+      return std::get<std::int64_t>(bus_signal_->raw);
   }
 
   return QVariant{};
