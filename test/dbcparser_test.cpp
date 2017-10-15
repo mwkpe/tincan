345aa44708a4fa47a6fd37f98507fe3540513f86
peter <xe@kthx.de>
2017-10-16 00:09:40 +0200

    Update tests to changed frame data

--- test/dbcparser_test.cpp
+++ test/dbcparser_test.cpp
@@ -14,39 +14,69 @@ TEST_CASE("DBC file parser")
   auto file = dbc::parse("test.dbc");
   CHECK(file.name == "test.dbc");
 
-  const auto* frame_def = dbc::find_frame_def(file, "MESSAGE");
-  REQUIRE(frame_def != nullptr);
-  CHECK(frame_def->dlc == 4);
-  CHECK(frame_def->id == 0xC9);
-  CHECK(frame_def->signal_defs.size() == 4);
+  const auto* veh_state_def = dbc::find_frame_def(file, "VEH_STATE");
+  REQUIRE(veh_state_def != nullptr);
+  CHECK(veh_state_def->dlc == 8);
+  CHECK(veh_state_def->id == 0xC9);
+  CHECK(veh_state_def->signal_defs.size() == 4);
 
-  SUBCASE("signal VEL")
+  SUBCASE("signal CRC")
   {
-    const auto* signal_def = dbc::find_signal_def(*frame_def, "VEL");
-    REQUIRE(signal_def != nullptr);
-    CHECK(signal_def->pos == 12);
-    CHECK(signal_def->len == 14);
-    CHECK(signal_def->order == dbc::Byte_order::Intel);
-    CHECK(signal_def->sign == dbc::Value_sign::Unsigned);
-    CHECK(Approx(signal_def->factor) == 0.01220703125);
-    CHECK(Approx(signal_def->offset) == 0);
-    CHECK(Approx(signal_def->minimum) == 0);
-    CHECK(Approx(signal_def->maximum) == 250);
-    CHECK(signal_def->unit == "km/h");
+    const auto* crc_def = dbc::find_signal_def(*veh_state_def, "CRC");
+    REQUIRE(crc_def != nullptr);
+    CHECK(crc_def->pos == 0);
+    CHECK(crc_def->len == 8);
+    CHECK(crc_def->order == dbc::Byte_order::Intel);
+    CHECK(crc_def->sign == dbc::Value_sign::Unsigned);
+    CHECK(Approx(crc_def->factor) == 1);
+    CHECK(Approx(crc_def->offset) == 0);
+    CHECK(Approx(crc_def->minimum) == 0);
+    CHECK(Approx(crc_def->maximum) == 255);
+    CHECK(crc_def->unit == "");
   }
 
-  SUBCASE("signal MODE")
+  SUBCASE("signal ALIV")
   {
-    const auto* signal_def = dbc::find_signal_def(*frame_def, "MODE");
-    REQUIRE(signal_def != nullptr);
-    CHECK(signal_def->pos == 26);
-    CHECK(signal_def->len == 6);
-    CHECK(signal_def->order == dbc::Byte_order::Intel);
-    CHECK(signal_def->sign == dbc::Value_sign::Unsigned);
-    CHECK(Approx(signal_def->factor) == 1);
-    CHECK(Approx(signal_def->offset) == 0);
-    CHECK(Approx(signal_def->minimum) == 10);
-    CHECK(Approx(signal_def->maximum) == 50);
-    CHECK(signal_def->unit == "");
+    const auto* aliv_def = dbc::find_signal_def(*veh_state_def, "ALIV");
+    REQUIRE(aliv_def != nullptr);
+    CHECK(aliv_def->pos == 8);
+    CHECK(aliv_def->len == 4);
+    CHECK(aliv_def->order == dbc::Byte_order::Intel);
+    CHECK(aliv_def->sign == dbc::Value_sign::Unsigned);
+    CHECK(Approx(aliv_def->factor) == 1);
+    CHECK(Approx(aliv_def->offset) == 0);
+    CHECK(Approx(aliv_def->minimum) == 0);
+    CHECK(Approx(aliv_def->maximum) == 15);
+    CHECK(aliv_def->unit == "");
+  }
+
+  SUBCASE("signal VELOCITY")
+  {
+    const auto* velocity_def = dbc::find_signal_def(*veh_state_def, "VELOCITY");
+    REQUIRE(velocity_def != nullptr);
+    CHECK(velocity_def->pos == 12);
+    CHECK(velocity_def->len == 14);
+    CHECK(velocity_def->order == dbc::Byte_order::Intel);
+    CHECK(velocity_def->sign == dbc::Value_sign::Unsigned);
+    CHECK(Approx(velocity_def->factor) == 0.01);
+    CHECK(Approx(velocity_def->offset) == 0);
+    CHECK(Approx(velocity_def->minimum) == 0);
+    CHECK(Approx(velocity_def->maximum) == 140);
+    CHECK(velocity_def->unit == "mph");
+  }
+
+  SUBCASE("signal WIPER_POSITION")
+  {
+    const auto* wiper_position_def = dbc::find_signal_def(*veh_state_def, "WIPER_POSITION");
+    REQUIRE(wiper_position_def != nullptr);
+    CHECK(wiper_position_def->pos == 26);
+    CHECK(wiper_position_def->len == 16);
+    CHECK(wiper_position_def->order == dbc::Byte_order::Intel);
+    CHECK(wiper_position_def->sign == dbc::Value_sign::Unsigned);
+    CHECK(Approx(wiper_position_def->factor) == 0.010986328125);
+    CHECK(Approx(wiper_position_def->offset) == 0);
+    CHECK(Approx(wiper_position_def->minimum) == 0);
+    CHECK(Approx(wiper_position_def->maximum) == 180);
+    CHECK(wiper_position_def->unit == "deg");
   }
 }
