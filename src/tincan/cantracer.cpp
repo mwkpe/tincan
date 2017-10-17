bec71af8cef6a1d0f27a5d427a3dd8afc003d59e
peter <xe@kthx.de>
2017-10-18 00:48:16 +0200

    Implement signal tracing

--- src/tincan/cantracer.cpp
+++ src/tincan/cantracer.cpp
@@ -3,6 +3,7 @@
 
 #include "fmt/fmtlib.h"
 
+#include "bussignal.h"
 #include "canframe.h"
 #include "canbus.h"
 
@@ -11,8 +12,16 @@ namespace
 {
 
 
-const auto trace_format = R"({},{:0>4},)"
-    R"(<font color="#33E7F7">{:0>8X}</font>,<font color="#23E02B">{}</font>,)"
+using namespace std::string_literals;
+
+
+const auto silver = "#C0C0C0"s;
+const auto magenta = "#FB4FE7"s;
+
+
+const auto frame_format = R"(<p style="white-space:pre"><font color="#808080">{}</font>,{:>4},)"
+    R"(<font color="#33E7F7">{:X}</font>,)"
+    R"(<font color="#23E02B">{}</font>,)"
     R"(<font color="{}">{:0>2X}</font>)"
     R"(<font color="{}">{:0>2X}</font>)"
     R"(<font color="{}">{:0>2X}</font>)"
@@ -21,7 +30,12 @@ const auto trace_format = R"({},{:0>4},)"
     R"(<font color="{}">{:0>2X}</font>)"
     R"(<font color="{}">{:0>2X}</font>)"
     R"(<font color="{}">{:0>2X}</font>)"
-    R"(,<font color="#FBED27">{}</font>)";
+    R"(,<font color="#FBED27">{}</font></p>)";
+
+const auto signal_format = R"(<p style="white-space:pre"><font color="#808080">{}</font>,{:>4},)"
+    R"(<font color="#33E7F7">{}</font>,)"
+    R"(<font color="#23E02B">{:>10}</font>,)"
+    R"(<font color="#FBED27">{}</font></p>)";
 
 
 }  // namespace
@@ -29,30 +43,50 @@ const auto trace_format = R"({},{:0>4},)"
 
 void tin::Can_tracer::update_data(std::uint32_t id)
 {
-  if (trace_frame_ && id == trace_frame_->id) {
-    auto line = QString::fromStdString(fmt::format(trace_format,
-        trace_frame_->receive_time,
-        prev_frame_.receive_time > 0 ? trace_frame_->receive_time - prev_frame_.receive_time : 0,
-        trace_frame_->id,
-        trace_frame_->frame_def ? trace_frame_->frame_def->dlc : 8,
-        trace_frame_->raw_data[7] != prev_frame_.raw_data[7] ? "#FB4FE7" : "#C0C0C0",
-        trace_frame_->raw_data[7],
-        trace_frame_->raw_data[6] != prev_frame_.raw_data[6] ? "#FB4FE7" : "#C0C0C0",
-        trace_frame_->raw_data[6],
-        trace_frame_->raw_data[5] != prev_frame_.raw_data[5] ? "#FB4FE7" : "#C0C0C0",
-        trace_frame_->raw_data[5],
-        trace_frame_->raw_data[4] != prev_frame_.raw_data[4] ? "#FB4FE7" : "#C0C0C0",
-        trace_frame_->raw_data[4],
-        trace_frame_->raw_data[3] != prev_frame_.raw_data[3] ? "#FB4FE7" : "#C0C0C0",
-        trace_frame_->raw_data[3],
-        trace_frame_->raw_data[2] != prev_frame_.raw_data[2] ? "#FB4FE7" : "#C0C0C0",
-        trace_frame_->raw_data[2],
-        trace_frame_->raw_data[1] != prev_frame_.raw_data[1] ? "#FB4FE7" : "#C0C0C0",
-        trace_frame_->raw_data[1],
-        trace_frame_->raw_data[0] != prev_frame_.raw_data[0] ? "#FB4FE7" : "#C0C0C0",
-        trace_frame_->raw_data[0],
-        trace_frame_->frame_def ? trace_frame_->frame_def->name : "FRAME"));
-        prev_frame_ = *trace_frame_;
+  if (trace_frame_ && !paused_ && id == trace_frame_->id) {
+    QString line;
+    if (trace_signal_) {
+      std::string physical_value;
+      if (std::holds_alternative<std::uint64_t>(trace_signal_->phys))
+        physical_value = fmt::format("{:>10}", std::get<std::uint64_t>(trace_signal_->phys));
+      else if (std::holds_alternative<std::int64_t>(trace_signal_->phys))
+        physical_value = fmt::format("{:>10}", std::get<std::int64_t>(trace_signal_->phys));
+      else
+        physical_value = fmt::format("{:>10f}", std::get<double>(trace_signal_->phys));
+
+      line = QString::fromStdString(fmt::format(signal_format,
+          trace_frame_->receive_time,
+          prev_frame_.receive_time > 0 ? trace_frame_->receive_time - prev_frame_.receive_time : 0,
+          physical_value,
+          std::holds_alternative<std::uint64_t>(trace_signal_->raw) ?
+              std::get<std::uint64_t>(trace_signal_->raw) : std::get<std::int64_t>(trace_signal_->raw),
+          trace_signal_->signal_def->name));
+    }
+    else {
+      line = QString::fromStdString(fmt::format(frame_format,
+          trace_frame_->receive_time,
+          prev_frame_.receive_time > 0 ? trace_frame_->receive_time - prev_frame_.receive_time : 0,
+          trace_frame_->id,
+          trace_frame_->frame_def ? trace_frame_->frame_def->dlc : 8,
+          trace_frame_->raw_data[7] != prev_frame_.raw_data[7] ? magenta : silver,
+          trace_frame_->raw_data[7],
+          trace_frame_->raw_data[6] != prev_frame_.raw_data[6] ? magenta : silver,
+          trace_frame_->raw_data[6],
+          trace_frame_->raw_data[5] != prev_frame_.raw_data[5] ? magenta : silver,
+          trace_frame_->raw_data[5],
+          trace_frame_->raw_data[4] != prev_frame_.raw_data[4] ? magenta : silver,
+          trace_frame_->raw_data[4],
+          trace_frame_->raw_data[3] != prev_frame_.raw_data[3] ? magenta : silver,
+          trace_frame_->raw_data[3],
+          trace_frame_->raw_data[2] != prev_frame_.raw_data[2] ? magenta : silver,
+          trace_frame_->raw_data[2],
+          trace_frame_->raw_data[1] != prev_frame_.raw_data[1] ? magenta : silver,
+          trace_frame_->raw_data[1],
+          trace_frame_->raw_data[0] != prev_frame_.raw_data[0] ? magenta : silver,
+          trace_frame_->raw_data[0],
+          trace_frame_->frame_def ? trace_frame_->frame_def->name : "FRAME"));
+    }
+    prev_frame_ = *trace_frame_;
     emit line_ready(line);
   }
 }
--- src/tincan/cantracer.h
+++ src/tincan/cantracer.h
@@ -12,23 +12,32 @@ namespace tin
 {
 
 
+class Bus_signal;
+
+
 class Can_tracer : public QObject
 {
   Q_OBJECT
 
 public:
   void set_frame(const Can_frame* trace_frame) { trace_frame_ = trace_frame;
-      prev_frame_.receive_time = 0; }
+      prev_frame_.receive_time = 0; trace_signal_ = nullptr; }
+  void set_signal(const Bus_signal* trace_signal, const Can_frame* trace_frame) {
+      trace_signal_ = trace_signal; trace_frame_ = trace_frame; prev_frame_.receive_time = 0; }
 
 signals:
   void line_ready(const QString&);
 
 public slots:
   void update_data(std::uint32_t id);
+  void set_paused(bool paused) { paused_ = paused; }
+  void reset() { set_frame(nullptr); }
 
 private:
   const Can_frame* trace_frame_ = nullptr;
+  const Bus_signal* trace_signal_ = nullptr;
   Can_frame prev_frame_;
+  bool paused_ = false;
 };
 
 
