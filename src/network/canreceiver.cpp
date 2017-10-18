8df3dcf8632b76977397b38d1789f7f73b1993f3
peter <xe@kthx.de>
2017-10-19 00:47:09 +0200

    Adapt receive code
    Gateway may now send a timestamp for each CAN frame for more precise
    timing information

--- src/network/canreceiver.cpp
+++ src/network/canreceiver.cpp
@@ -6,12 +6,19 @@
 
 void can::Receiver::handle_receive(gsl::span<std::uint8_t> buffer)
 {
-  using namespace std::chrono;
-
-  if (buffer.size() == sizeof(can::Raw_frame)) {
+  if (buffer.size() == sizeof(std::uint64_t) + sizeof(can::Raw_frame)) {
+    // Received CAN frame with a timestamp from the CAN receiver
+    auto* time = reinterpret_cast<std::uint64_t*>(buffer.data());
+    auto* frame = reinterpret_cast<can::Raw_frame*>(buffer.data() + sizeof(std::uint64_t));
+    emit received_frame(*time, *frame);
+    emit received_frame_id(frame->id);
+  }
+  else if (buffer.size() == sizeof(can::Raw_frame)) {
+    // Time may be imprecise and fluctuate +-15 ms on every second full moon
+    using namespace std::chrono;
+    auto time = duration_cast<milliseconds>(high_resolution_clock::now().time_since_epoch()).count();
     auto* frame = reinterpret_cast<can::Raw_frame*>(buffer.data());
-    emit received_frame(duration_cast<milliseconds>(high_resolution_clock::now()
-        .time_since_epoch()).count(), *frame);
+    emit received_frame(time, *frame);
     emit received_frame_id(frame->id);
   }
 }
