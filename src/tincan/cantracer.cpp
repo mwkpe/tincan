24599d296a62eb84cccf42d90d44af787cfb379a
peter <xe@kthx.de>
2017-10-27 23:29:50 +0200

    Restructure code

--- src/tincan/cantracer.cpp
+++ src/tincan/cantracer.cpp
@@ -38,6 +38,54 @@ const auto signal_format = R"(<p style="white-space:pre"><font color="#808080">{
     R"(<font color="#FBED27">{}</font></p>)";
 
 
+std::string to_string(const tin::Can_frame* frame, const tin::Can_frame* prev_frame)
+{
+  return fmt::format(frame_format,
+      frame->receive_time,
+      prev_frame->receive_time > 0 ? frame->receive_time - prev_frame->receive_time : 0,
+      frame->id,
+      frame->frame_def ? frame->frame_def->dlc : 8,
+      frame->raw_data[7] != prev_frame->raw_data[7] ? magenta : silver,
+      frame->raw_data[7],
+      frame->raw_data[6] != prev_frame->raw_data[6] ? magenta : silver,
+      frame->raw_data[6],
+      frame->raw_data[5] != prev_frame->raw_data[5] ? magenta : silver,
+      frame->raw_data[5],
+      frame->raw_data[4] != prev_frame->raw_data[4] ? magenta : silver,
+      frame->raw_data[4],
+      frame->raw_data[3] != prev_frame->raw_data[3] ? magenta : silver,
+      frame->raw_data[3],
+      frame->raw_data[2] != prev_frame->raw_data[2] ? magenta : silver,
+      frame->raw_data[2],
+      frame->raw_data[1] != prev_frame->raw_data[1] ? magenta : silver,
+      frame->raw_data[1],
+      frame->raw_data[0] != prev_frame->raw_data[0] ? magenta : silver,
+      frame->raw_data[0],
+      frame->frame_def ? frame->frame_def->name : "FRAME");
+}
+
+
+std::string to_string(const tin::Can_frame* frame, const tin::Can_frame* prev_frame,
+    const tin::Bus_signal* signal)
+{
+  std::string physical_value;
+  if (std::holds_alternative<std::uint64_t>(signal->phys))
+    physical_value = fmt::format("{:>10}", std::get<std::uint64_t>(signal->phys));
+  else if (std::holds_alternative<std::int64_t>(signal->phys))
+    physical_value = fmt::format("{:>10}", std::get<std::int64_t>(signal->phys));
+  else
+    physical_value = fmt::format("{:>10f}", std::get<double>(signal->phys));
+
+  return fmt::format(signal_format,
+      frame->receive_time,
+      prev_frame->receive_time > 0 ? frame->receive_time - prev_frame->receive_time : 0,
+      physical_value,
+      std::holds_alternative<std::uint64_t>(signal->raw) ? std::get<std::uint64_t>(signal->raw) :
+          std::get<std::int64_t>(signal->raw),
+      signal->signal_def->name);
+}
+
+
 }  // namespace
 
 
@@ -45,47 +93,10 @@ void tin::Can_tracer::update_data(std::uint32_t id)
 {
   if (trace_frame_ && !paused_ && id == trace_frame_->id) {
     QString line;
-    if (trace_signal_) {
-      std::string physical_value;
-      if (std::holds_alternative<std::uint64_t>(trace_signal_->phys))
-        physical_value = fmt::format("{:>10}", std::get<std::uint64_t>(trace_signal_->phys));
-      else if (std::holds_alternative<std::int64_t>(trace_signal_->phys))
-        physical_value = fmt::format("{:>10}", std::get<std::int64_t>(trace_signal_->phys));
-      else
-        physical_value = fmt::format("{:>10f}", std::get<double>(trace_signal_->phys));
-
-      line = QString::fromStdString(fmt::format(signal_format,
-          trace_frame_->receive_time,
-          prev_frame_.receive_time > 0 ? trace_frame_->receive_time - prev_frame_.receive_time : 0,
-          physical_value,
-          std::holds_alternative<std::uint64_t>(trace_signal_->raw) ?
-              std::get<std::uint64_t>(trace_signal_->raw) : std::get<std::int64_t>(trace_signal_->raw),
-          trace_signal_->signal_def->name));
-    }
-    else {
-      line = QString::fromStdString(fmt::format(frame_format,
-          trace_frame_->receive_time,
-          prev_frame_.receive_time > 0 ? trace_frame_->receive_time - prev_frame_.receive_time : 0,
-          trace_frame_->id,
-          trace_frame_->frame_def ? trace_frame_->frame_def->dlc : 8,
-          trace_frame_->raw_data[7] != prev_frame_.raw_data[7] ? magenta : silver,
-          trace_frame_->raw_data[7],
-          trace_frame_->raw_data[6] != prev_frame_.raw_data[6] ? magenta : silver,
-          trace_frame_->raw_data[6],
-          trace_frame_->raw_data[5] != prev_frame_.raw_data[5] ? magenta : silver,
-          trace_frame_->raw_data[5],
-          trace_frame_->raw_data[4] != prev_frame_.raw_data[4] ? magenta : silver,
-          trace_frame_->raw_data[4],
-          trace_frame_->raw_data[3] != prev_frame_.raw_data[3] ? magenta : silver,
-          trace_frame_->raw_data[3],
-          trace_frame_->raw_data[2] != prev_frame_.raw_data[2] ? magenta : silver,
-          trace_frame_->raw_data[2],
-          trace_frame_->raw_data[1] != prev_frame_.raw_data[1] ? magenta : silver,
-          trace_frame_->raw_data[1],
-          trace_frame_->raw_data[0] != prev_frame_.raw_data[0] ? magenta : silver,
-          trace_frame_->raw_data[0],
-          trace_frame_->frame_def ? trace_frame_->frame_def->name : "FRAME"));
-    }
+    if (trace_signal_)
+      line = QString::fromStdString(to_string(trace_frame_, &prev_frame_, trace_signal_));
+    else
+      line = QString::fromStdString(to_string(trace_frame_, &prev_frame_));
     prev_frame_ = *trace_frame_;
     emit line_ready(line);
   }
