2262eb214be951cb678f16c00dd2c8888d875894
mwkpe <mwkpe@proton.me>
2025-06-15 00:58:46 +0200

    Fix indexing bug, refactor code

--- src/models/canbusmodel.cpp
+++ src/models/canbusmodel.cpp
@@ -7,7 +7,6 @@
 #include <QColor>
 #include <QBrush>
 
-#include "tincan/cansignal.h"
 #include "tincan/canframe.h"
 #include "tincan/canbus.h"
 #include "models/treeitemid.h"
@@ -21,15 +20,19 @@ tin::Can_bus_model::Can_bus_model(const Can_bus* can_bus, QObject* parent)
 {
   construct();
 
-  // Deferred update of tree items since multiple frames with low cycle times cause a CPU usage
-  // spike due to an excessive amount of data update calls. Be aware, depening on the refresh
-  // rate and a frame's cycle time this may lead to some changes not being displayed!
+  // Deferred update of tree items since multiple frames with low cycle times cause high CPU usage
+  // due to an excessive amount of data update calls. Be aware, depening on the refresh rate and
+  // a frame's cycle time this may lead to some changes not being displayed
   auto* update_timer = new QTimer{this};
+
   connect(update_timer, &QTimer::timeout, this, [this]{
-    for (auto id : deferred_updates_)
+    for (auto id : deferred_updates_) {
       update_data(id);
+    }
+
     deferred_updates_.clear();
   });
+
   update_timer->start(53);
 }
 
@@ -43,18 +46,23 @@ void tin::Can_bus_model::construct()
 
 QVariant tin::Can_bus_model::data(const QModelIndex& index, int role) const
 {
-  if (!index.isValid())
+  if (!index.isValid()) {
     return QVariant{};
+  }
 
   switch (role) {
     case Qt::ForegroundRole: {
       auto* item = reinterpret_cast<Tree_item*>(index.internalPointer());
       if (item->id() == Item_id::Can_frame) {
         const auto* frame_item = reinterpret_cast<const Can_frame_item*>(item);
-        if (!frame_item->frame()->alive)
-          return QBrush{QColor{"#787878"}};
-        if (item->child_count() > 0)
-          return QBrush{QColor{"#33E7F7"}};
+
+        if (!frame_item->frame()->alive) {
+          return QBrush{QColor{0x787878}};
+        }
+
+        if (item->child_count() > 0) {
+          return QBrush{QColor{0x33E7F7}};
+        }
       }
       return Tree_model::data(index, role);
     }
@@ -83,14 +91,24 @@ void tin::Can_bus_model::update_data(std::uint32_t frame_id)
 {
   if (auto* item = find_item(frame_id); item) {  // Update existing frame
     auto row = item->row();
-    if (row >= 0)
-      dataChanged(index(row, 0), index(row, columnCount()), QVector<int>{Qt::DisplayRole});
+    if (row >= 0) {
+      // Update frame
+      auto parent_index = index(row, 0);
+      emit dataChanged(parent_index, index(row, columnCount() - 1), QVector<int>{Qt::DisplayRole});
+
+      // Update signals
+      if (int child_count = rowCount(parent_index); child_count > 0) {
+        emit dataChanged(index(0, 0, parent_index), index(child_count - 1,
+            columnCount() - 1, parent_index));
+      }
+    }
   }
   else {  // Add new frame
     if (auto* frame = can_bus_->frame(frame_id); frame) {
       auto frame_item = std::make_unique<Can_frame_item>(frame, root_item_.get());
-      for (const auto& signal : frame->can_signals)
+      for (const auto& signal : frame->can_signals) {
         frame_item->add_child(std::make_unique<Can_signal_item>(&signal, frame_item.get()));
+      }
       frame_items_.insert({frame_id, frame_item.get()});
       beginInsertRows(QModelIndex{}, rowCount(), rowCount());
       root_item_->add_child(std::move(frame_item));
@@ -103,7 +121,10 @@ void tin::Can_bus_model::update_data(std::uint32_t frame_id)
 tin::Can_frame_item* tin::Can_bus_model::find_item(std::uint32_t frame_id)
 {
   auto it = frame_items_.find(frame_id);
-  if (it != std::end(frame_items_))
+
+  if (it != std::end(frame_items_)) {
     return it->second;
+  }
+
   return nullptr;
 }
